7102c72944a09c0b70de34c7a689ec7c61c01d21
Jonny Spry <j.spry@arts.ac.uk>
2021-01-07 16:00:13 +0000

    add client code

---
+++ client-fe/src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import './App.css';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: null
+		}
+
+	}
+
+	componentDidMount() {
+		console.log('componentDidMount')
+		fetch("http://localhost:5000")
+			.then(response => response.json())
+			.then(data => {
+				this.setState({
+					data: data.res
+				})
+				console.log('this.state', this.state)
+			})
+	}
+	render(){ 
+		return (
+			<div className="App">
+				<header className="App-header">
+					<p>
+						I am here! and so is someone else...........
+					</p>
+					<p>
+						{this.state.data}
+					</p>
+				</header>
+			</div>
+		)
+	}
+}
+
+export default App;
